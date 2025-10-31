6676c8480a176e36ccbe8240592e943aa55059b7
Anshuman Panda <ichuk0078@gmail.com>
2025-10-31 18:23:20 +0000

    added the user auth info. schema

---
+++ backend/src/models/User.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  googleId: { type: String, index: true },
+  email: { type: String, unique: true, sparse: true },
+  name: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+// export type UserDoc = mongoose.InferSchemaType<typeof UserSchema> & { _id: mongoose.Types.ObjectId };
+export const UserModel = mongoose.model('User', UserSchema);
+
+
+
+
